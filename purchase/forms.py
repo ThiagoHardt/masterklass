f283fa43ce6161d9e3d8b80362ee8c54df2f5712
Thiago Hardt <thiago.hardts@gmail.com>
2021-01-06 14:21:15 +0000

    Added purchase_plan template and form

---
+++ purchase/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from django.contrib.auth.models import User
+
+
+class SignupForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email',)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'username': 'Username',
+            'email': 'Email Address',
+        }
+
+        self.fields['username'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = False
