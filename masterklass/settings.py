4ff75e71c3d9ee5915ff3d94839008b492c70be4
Thiago Hardt <thiago.hardts@gmail.com>
2021-01-08 13:27:42 +0000

    Added login functionality

--- masterklass/settings.py
+++ masterklass/settings.py
@@ -71,6 +71,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
             os.path.join(BASE_DIR, 'templates', 'templates')
         ],
         'APP_DIRS': True,
