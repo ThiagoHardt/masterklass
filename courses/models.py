d59f7d2668f01df0f45664402217648cee313469
Thiago Hardt <thiago.hardts@gmail.com>
2021-01-20 11:32:29 +0000

    Updated Courses app models and docstrings

--- courses/models.py
+++ courses/models.py
@@ -3,23 +3,35 @@ from django.contrib.auth.models import User
 
 
 class Category(models.Model):
+    """
+    Categories are defined by this model.
+
+    fields: slug and name
+    required: slug and name
+    """
 
     class Meta:
         verbose_name_plural = 'Categories'
         ordering = ('name',)
 
     slug = models.SlugField()
-    name = models.CharField(max_length=255, null=True, blank=True)
+    name = models.CharField(max_length=255, null=True)
 
     def __str__(self):
         return self.name
 
 
 class Course(models.Model):
+    """
+    Courses are defined by this model.
+
+    fields: category, title, description and thumbnail
+    required: category and  title 
+    """
     category = models.ForeignKey(
-        "Category", null=True, blank=True, on_delete=models.SET_NULL)
+        "Category", null=True, on_delete=models.SET_NULL)
     title = models.CharField(max_length=155)
-    description = models.TextField(max_length=1024)
+    description = models.TextField(max_length=1024, blank=True, null=True)
     thumbnail = models.ImageField(
         default="default-image.png", upload_to='media')
 
@@ -28,9 +40,14 @@ class Course(models.Model):
 
 
 class Lesson(models.Model):
+    """
+    Lessons are defined by this model.
 
+    fields: slug, title, course, position, video_url and add_on
+    required: slug, title, course, position 
+    """
     class Meta:
-        ordering = ("position",)
+        ordering = ("add_on",)
 
     slug = models.SlugField()
     title = models.CharField(max_length=155)
@@ -44,6 +61,12 @@ class Lesson(models.Model):
 
 
 class Comment(models.Model):
+    """
+    Comments are defined by this model.
+
+    fields: course, author, body, and created_on
+    required: course, author, body  
+    """
     course = models.ForeignKey(
         Course, on_delete=models.CASCADE, related_name='comments', default=None)
     author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
