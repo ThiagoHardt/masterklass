2207096fb39f8af60e0c103c5573ea49e1ffc563
Thiago Hardt <thiago.hardts@gmail.com>
2021-01-21 16:06:26 +0000

    Updated media path on settings

--- profiles/models.py
+++ profiles/models.py
@@ -16,7 +16,8 @@ class UserProfile(models.Model):
                              on_delete=models.SET_NULL, limit_choices_to={'active': True})
     date_joined = models.DateTimeField(auto_now=True, null=True)
     name = models.CharField(max_length=255, null=True, blank=True)
-    profile_picture = models.ImageField()
+    profile_picture = models.ImageField(
+        default="default-profile-picture.jpg", upload_to='profile_img')
 
     def __str__(self):
         return self.user.username
